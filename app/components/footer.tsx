3d7c70aa4aca1aa2d28b8a9dc4454fe58b955033
Fury Oktria Putra <91455732+furyoktria@users.noreply.github.com>
2024-09-15 19:01:33 +0700

    Update footer.tsx
    Change footer detail

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,10 +35,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://x.com/furyoktria"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
+            <p className="ml-2 h-7">Fury: Twitter</p>
           </a>
         </li>
         <li>
@@ -46,10 +46,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://www.linkedin.com/in/furyoktria/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">Fury: LinkedIn</p>
           </a>
         </li>
       </ul>
