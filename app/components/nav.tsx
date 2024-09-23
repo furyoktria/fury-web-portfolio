bd3d1d42d6a9e1ac70c591736106d82ca8c83cbf
Fury Oktria Putra <91455732+furyoktria@users.noreply.github.com>
2024-09-23 13:48:36 +0700

    navigation hierrarchy

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,14 +4,14 @@ const navItems = {
   '/': {
     name: 'home',
   },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://bit.ly/PortofolioFury': {
-    name: 'product portfolio',
-  },
   '/expertise': {
     name: 'expertise',
+  },
+  'https://bit.ly/PortofolioFury': {
+    name: 'portfolio',
+  },
+  '/blog': {
+    name: 'blog',
   },
 }
 
